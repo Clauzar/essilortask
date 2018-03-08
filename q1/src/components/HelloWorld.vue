4e2a3dedfd141b135bb96d0965d919ec3a5b76b0
Thammawit <vanguardeak@hotmail.com>
2018-03-08 18:42:44 +0700

    Question 1-2

---
+++ q1/src/components/HelloWorld.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="hello">
+    <b-navbar toggleable="md" type="dark" variant="dark">
+
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+      
+      <b-navbar-brand href="#"><img src="../assets/logo.png" style="width:75px;height:50px;"></b-navbar-brand>
+
+      <b-collapse is-nav id="nav_collapse">
+      <b-nav-item href="#">Contact Sales</b-nav-item>    
+        <b-nav-item-dropdown text="Products" right>
+          <b-dropdown-item href="#">1</b-dropdown-item>
+          <b-dropdown-item href="#">2</b-dropdown-item>
+          <b-dropdown-item href="#">3</b-dropdown-item>
+          <b-dropdown-item href="#">4</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+      <b-nav-item href="#">Solutions</b-nav-item>
+      <b-nav-item href="#">Pricing</b-nav-item>
+      <b-nav-item href="#">Getting Start</b-nav-item>
+
+        <b-nav-item-dropdown text="More" right>
+        <b-dropdown-item href="#">1</b-dropdown-item>
+        <b-dropdown-item href="#">2</b-dropdown-item>
+        <b-dropdown-item href="#">3</b-dropdown-item>
+        <b-dropdown-item href="#">4</b-dropdown-item>
+      </b-nav-item-dropdown>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+
+          <b-nav-item-dropdown text="English" right>
+            <b-dropdown-item href="#">EN</b-dropdown-item>
+            <b-dropdown-item href="#">TH</b-dropdown-item>
+            <b-dropdown-item href="#">RU</b-dropdown-item>
+            <b-dropdown-item href="#">FA</b-dropdown-item>
+          </b-nav-item-dropdown>
+
+          <b-nav-item-dropdown right>
+            <!-- Using button-content slot -->
+            <template slot="button-content">
+              My Account
+            </template>
+            <b-dropdown-item href="#">Management Console</b-dropdown-item>
+            <b-dropdown-item href="#">Account </b-dropdown-item>
+            <b-dropdown-item href="#">Billing</b-dropdown-item>
+            <b-dropdown-item href="#">Security </b-dropdown-item>
+            <b-dropdown-item href="#">Personal </b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+
+        <b-button size="sm" class="warning" type="submit">Sign Into the Console</b-button>
+
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+/* h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+} */
+</style>
---
+++ q1/src/components/bod.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="bod">
+    <h1>Contact</h1>
+ <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group id="exampleInputGroup1"
+                    label="Email address:"
+                    label-for="exampleInput1"
+                    description="Please Provide your company email address to allow us to route to your inquiry to the right contact">
+        <b-form-input id="exampleInput1"
+                      type="email"
+                      v-model="form.email"
+                      required
+                      placeholder="Enter email"
+                      >
+        </b-form-input>
+      </b-form-group>
+      <b-form-group id="fname"
+                    label="First Name:"
+                    label-for="fname">
+        <b-form-input id="fname"
+                      type="text"
+                      v-model="form.name"
+                      required
+                      placeholder="Enter name">
+        </b-form-input>
+      </b-form-group>
+      <b-form-group id="lname"
+                    label="Last Name:"
+                    label-for="lname">
+        <b-form-select id="lname"
+                      :options="foods"
+                      required
+                      v-model="form.food">
+        </b-form-select>
+      </b-form-group>
+      Job Role:
+      <b-form-select v-model="selected" :options="options" class="mb-3" />
+
+      <b-form-group id="tel"
+                    label="Telephone:"
+                    label-for="tel">
+        <b-form-select id="tel"
+                      :options="foods"
+                      required
+                      v-model="form.food">
+        </b-form-select>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+    </b-form>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      selected: null,
+      options: [
+        { value: null, text: 'Please select an option' },
+        { value: 'a', text: '1' },
+        { value: 'b', text: '2' },
+      ],
+      form: {
+        email: '',
+        name: '',
+        food: null,
+        checked: []
+      },
+      foods: [
+        { text: 'Select One', value: null },
+        'Carrots', 'Beans', 'Tomatoes', 'Corn'
+      ],
+      show: true
+    }
+  },
+  methods: {
+    onSubmit (evt) {
+      evt.preventDefault();
+      alert(JSON.stringify(this.form));
+    },
+    onReset (evt) {
+      evt.preventDefault();
+      /* Reset our form values */
+      this.form.email = '';
+      this.form.name = '';
+      this.form.food = null;
+      this.form.checked = [];
+      /* Trick to reset/clear native browser form validation state */
+      this.show = false;
+      this.$nextTick(() => { this.show = true });
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
+
+    
